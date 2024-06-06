505277f7425ff0527b88ed50e9ddd5f0a6481698
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-06-06 18:45:11 +0200

    Debuging after merging project page branch

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,18 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage/LandingPage";
-import Project1 from "./components/ProjectPage/Project1";
-import Project2 from "./components/ProjectPage/Project2";
-import Project3 from "./components/ProjectPage/Project3";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage"
+import Project from "./components/ProjectPage/Project";
 
 function App() {
   return (
-    <div className="font-display">
-      <BrowserRouter>
+    <Router>
+      <div className="App">
         <Routes>
-          <Route element={<LandingPage />} path="/" />
-          <Route element={<Project1 />} path="/project1" />
-          <Route element={<Project2 />} path="/project2" />
-          <Route element={<Project3 />} path="/project3" />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/project" element={<Project />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </Router>
   );
 }
 
--- portfolio/src/components/LandingPage/LandingPage.jsx
+++ portfolio/src/components/LandingPage/LandingPage.jsx
@@ -2,17 +2,40 @@ import React from "react"
 import { NavLink } from "react-router-dom"
 
 function LandingPage() {
+  //Docelowo będą to dane z backednu / CMS przesyłane w podobnej lub identycznej strukturze.
+  const project1 = {
+    name: "Pegasus Solar",
+    imageLink: `${process.env.PUBLIC_URL}/assets/images/Pegasus/PegasusHero.png`,
+    description: {
+      type: ["Landing Page", "Redesign"],
+      stage: "Development in progress",
+      deliverables: [
+        "Vision & Strategy",
+        "UX Research",
+        "Rebranding",
+        "Testing",
+      ],
+    },
+  }
+
+  const project2 = {
+    name: "NetPartners",
+    imageLink: `${process.env.PUBLIC_URL}/assets/images/NetPartners/NetPartnersHero.png`,
+    description: {
+      type: ["Product"],
+      stage: "Start up",
+      deliverables: ["Vision & Strategy", "Branding", "Product Design"],
+    },
+  }
+
   return (
     <div className="font-display">
       <h1>LandingPage</h1>
-      <NavLink to="/project1">
-        <li className=" cursor-pointer hover:opacity-15">Project 1</li>
+      <NavLink to="/project" state={project1}>
+        <li className="cursor-pointer hover:opacity-15">{project1.name}</li>
       </NavLink>
-      <NavLink to="/project2">
-        <li className="cursor-pointer hover:opacity-15">Project 2</li>
-      </NavLink>
-      <NavLink to="/project3">
-        <li className="cursor-pointer hover:opacity-15">Project 3</li>
+      <NavLink to="/project" state={project2}>
+        <li className="cursor-pointer hover:opacity-15">{project2.name}</li>
       </NavLink>
     </div>
   )
