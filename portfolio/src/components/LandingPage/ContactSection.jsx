2a5abf876469bd3bd2c29532bfea8dbe6f8e7a69
nathess <68251691+nathess@users.noreply.github.com>
2024-06-10 01:55:17 +0200

    Contact section links added

--- portfolio/src/components/LandingPage/ContactSection.jsx
+++ portfolio/src/components/LandingPage/ContactSection.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Element } from "react-scroll";
 
 function ContactSection() {
+  const mailContent =
+    "&body=Hello Karolina, %0D%0A %0D%0A I'm reaching out because I'm  interested in collaborating with you on a design project. When are you available for a brief call or meeting to explore this opportunity in more detail %0D%0A %0D%0A Kindest,...";
+
   return (
     <Element name="contact">
       <div className="bg-[#2C2D30] md:px-[60px] pb-[60px]">
@@ -10,12 +13,31 @@ function ContactSection() {
             don't be shy
           </p>
           <p className="text-[#ECECEE] text-[95px] mb-[20px]">
-            Say hello <span className="text-[16px]">(email)</span>
+            Say hello
+            <a
+              href={`mailto:hesskarolina@gmail.com ?subject=Collaboration ${mailContent}`}
+              className="text-[16px] ml-[20px]"
+            >
+              (email)
+            </a>
           </p>
           <p className="text-[#85878A] text-[25px] font-bold uppercase">or</p>
-          <p className="text-[#ECECEE] text-[95px] ml-[120px]">
-            spy on me here <span className="text-[16px]">(LinkedIn)</span>
-            <span className="text-[16px]">(Instagram)</span>
+          <p className="text-[#ECECEE] text-[95px] ml-[120px] md:leading-[2rem]">
+            spy on me here
+            <span className="inline-block ml-[20px]">
+              <a
+                href="https://www.linkedin.com/in/karolina-hess-793399257/"
+                className="text-[16px] block"
+              >
+                (LinkedIn)
+              </a>
+              <a
+                href="https://www.instagram.com/karo.dsgn"
+                className="text-[16px] block"
+              >
+                (Instagram)
+              </a>
+            </span>
           </p>
         </div>
         <div className="flex justify-between text-[12px] pt-[12px] border-t-[.5px] border-[#67696D]">
