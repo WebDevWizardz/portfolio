ddefb504c2267c53e582163ffa28dc11e44bcac3
nathess <68251691+nathess@users.noreply.github.com>
2024-06-10 19:19:28 +0200

    header text color change depending on bg color added

--- portfolio/src/components/LandingPage/Header.jsx
+++ portfolio/src/components/LandingPage/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-scroll";
 
 function Header() {
   return (
-    <nav className="fixed z-50 w-full  py-[20px] px-[20px] md:px-[60px] md:py-[40px] flex justify-between">
+    <nav className="fixed z-50 w-full  py-[20px] px-[20px] md:px-[60px] md:py-[40px] flex justify-between mix-blend-difference text-[#fcfcfc]">
       <a href="#" className="w-[139px] h-[15,44px]">
-        <Logo fill="#494B50" />
+        <Logo fill="#fcfcfc" />
       </a>
-      <div className="flex gap-6 text-[12px] text-[#2C2D30] cursor-pointer">
+      <div className="flex gap-6 text-[12px] cursor-pointer">
         <Link
           className="text-bottom-line"
           to="work"
