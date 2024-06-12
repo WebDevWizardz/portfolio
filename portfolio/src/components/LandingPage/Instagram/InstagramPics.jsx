6cd59bc8593f5be379bca7f5bc73b98d163521ed
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-06-12 10:26:02 +0200

    Configurate and style Slider

--- portfolio/src/components/LandingPage/Instagram/InstagramPics.jsx
+++ portfolio/src/components/LandingPage/Instagram/InstagramPics.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react"
+import Swiper from "swiper/bundle"
+import "swiper/css"
+import "swiper/css/navigation" // Import Swiper navigation styles
+import "swiper/css/pagination" // Import Swiper pagination styles
 
 function InstagramPics({ images }) {
+  useEffect(() => {
+    new Swiper(".swiper", {
+      direction: "horizontal",
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 20,
+      breakpoints: {
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+    })
+  }, [])
+
   return (
-    <div className="flex gap-x-5">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className={`w-[315px] ${
-            index % 2 === 0 ? "h-[315px]" : "h-[420px]"
-          } mb-5`}
-        >
-          <img
-            className="object-cover w-full h-full"
-            src={`${process.env.PUBLIC_URL}${image.src}`}
-            alt={image.alt}
-          />
-        </div>
-      ))}
+    <div className="swiper">
+      <div className="swiper-wrapper">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`swiper-slide w-[315px] ${
+              index % 2 === 0 ? "h-[315px]" : "h-[420px]"
+            }`}
+          >
+            <img
+              className="object-cover"
+              src={`${process.env.PUBLIC_URL}${image.src}`}
+              alt={image.alt}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default InstagramPics;
+export default InstagramPics
--- portfolio/src/components/LandingPage/Instagram/InstagramSection.jsx
+++ portfolio/src/components/LandingPage/Instagram/InstagramSection.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import InstagramPics from "./InstagramPics";
+import React from "react"
+import InstagramPics from "./InstagramPics"
 
 function InstagramSection() {
   const images = [
@@ -19,16 +19,33 @@ function InstagramSection() {
       src: "/assets/images/Instagram/instagram-4.png",
       alt: "Instagram post 4",
     },
-    // Dodaj więcej obrazów tutaj
-  ];
+    {
+      src: "/assets/images/Instagram/instagram-1.png",
+      alt: "Instagram post 1",
+    },
+    {
+      src: "/assets/images/Instagram/instagram-2.png",
+      alt: "Instagram post 2",
+    },
+    {
+      src: "/assets/images/Instagram/instagram-3.png",
+      alt: "Instagram post 3",
+    },
+    {
+      src: "/assets/images/Instagram/instagram-4.png",
+      alt: "Instagram post 4",
+    },
+  ]
 
   return (
     <div className="flex flex-col items-center">
       <p className="text-[12px] text-[#85878A] mb-[20px]">instagram</p>
       <p className="text-[45px] text-[#2C2D30] mb-[55px]">@KARO.DSGN</p>
-      <InstagramPics images={images} />
+      <div className="w-full flex justify-center h-[500px]">
+        <InstagramPics images={images} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default InstagramSection;
+export default InstagramSection
