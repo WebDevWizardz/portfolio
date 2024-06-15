ded7fe0c76c6c9c602f97e7e94e282bb115bf84e
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-06-15 15:14:45 +0200

    Loader

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage"
+import Loader from "./components/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }, [])
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
+        {isLoading ? (<Loader />) : (
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        )}
       </div>
     </Router>
   );
