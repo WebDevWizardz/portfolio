481ffc72c06c0489687d2e50bbb7501c1a3f40db
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-08-01 18:06:22 +0200

    Scroll to top on every page refresh

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage"
 
 
 
 function App() {
+
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual'
+  }, [])
+
+
   return (
     <Router basename="/">
+
+
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
