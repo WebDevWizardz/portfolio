484f951ae163a0cf5f3a6f50e7fbc874e8ac9d18
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-08-09 15:47:27 +0200

    AutoPlay on different devices/browsers

--- portfolio/src/components/LandingPage/Hero.jsx
+++ portfolio/src/components/LandingPage/Hero.jsx
@@ -1,25 +1,53 @@
 import { gsap } from "gsap"
-import { useGSAP } from "@gsap/react"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { useEffect } from "react"
-import React from "react"
+import React, { useRef, useEffect, useState } from "react"
 import Logo from "./Logo"
-import useSanity from "../../customHooks/useSanity"
 import useCheckDeviceType from "../../customHooks/useCheckDeviceType"
 
 gsap.registerPlugin(ScrollTrigger)
 
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase()
+  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0
+}
+
 function Hero() {
-  const { reelData } = useSanity()
   const isMobile = useCheckDeviceType()
+  const videoParentRef = useRef(null)
+  const [shouldUseImage, setShouldUseImage] = useState(false)
 
   const mobileUrl =
     "https://cdn.sanity.io/files/lezkfw80/production/ca4967eac9ec5812abc27d3b5b8fa6595018964c.mp4"
   const desktopUrl =
     "https://cdn.sanity.io/files/lezkfw80/production/b41f4d841093db2c651dd9aa66e3b0e5a1d4d995.mp4"
-  console.log(desktopUrl)
+  const videoUrl = isMobile ? mobileUrl : desktopUrl
 
-  useGSAP(() => {
+  useEffect(() => {
+    if (isSafari() && videoParentRef.current) {
+      const player = videoParentRef.current.children[0]
+      if (player) {
+        player.controls = false
+        player.playsinline = true
+        player.muted = true
+        player.setAttribute("muted", "")
+        player.autoplay = true
+
+        setTimeout(() => {
+          const promise = player.play()
+          if (promise && promise.then) {
+            promise
+              .then(() => {
+                console.log("Video is playing successfully.")
+              })
+              .catch(() => {
+                videoParentRef.current.style.display = "none"
+                setShouldUseImage(true)
+              })
+          }
+        }, 0)
+      }
+    }
+
     gsap.to(".logo_animate", {
       scrollTrigger: {
         scrub: 0.5,
@@ -40,15 +68,32 @@ function Hero() {
       id="heroSection"
       className="z-10 heroSection relative h-[100vh] md:h-screen w-screen overflow-hidden bg-none start"
     >
-      <video
-        className="absolute w-screen h-full object-fit md:object-cover scale-150 sm:scale-1"
-        autoPlay
-        loop
-        muted
-      >
-        <source src={isMobile ? mobileUrl : desktopUrl} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      {shouldUseImage ? (
+        <img
+          src={videoUrl}
+          alt="Fallback"
+          className="absolute w-screen h-full object-fit md:object-cover scale-150 sm:scale-1"
+        />
+      ) : (
+        <div
+          ref={videoParentRef}
+          className="absolute w-screen h-full object-fit md:object-cover scale-150 sm:scale-1"
+          dangerouslySetInnerHTML={{
+            __html: `
+            <video
+              loop
+              muted
+              autoplay
+              playsinline
+              preload="metadata"
+              class="absolute w-screen h-full object-fit md:object-cover scale-150 sm:scale-1"
+            >
+            <source src="${videoUrl}" type="video/mp4" />
+            </video>`,
+          }}
+        />
+      )}
+
       <div className="relative z-10 flex items-end justify-center h-full p-5 mix-blend-difference">
         <div className="w-full absolute">
           <div className="logo-container">
